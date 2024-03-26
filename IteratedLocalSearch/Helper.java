b3ac57d5dd2cdb1d994a4f9ceb3350101288081b
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2024-03-26 23:38:14 +0000

    Constructor: Reads in a file and returns an ArrayList of Integers

--- IteratedLocalSearch/Helper.java
+++ IteratedLocalSearch/Helper.java
@@ -6,5 +6,40 @@ import java.io.*;
 
 public class Helper {
 
+    /**
+    * Reads in a file and returns an ArrayList of Integers
+    * 
+    * @param dirName
+    * @return fileContents
+    */
 
+    public static HashMap<String, ArrayList<Integer>> readFiles(String dirName) {
+
+        File dir = new File("1BPP_Instances/" + dirName);
+        File[] files = dir.listFiles();
+
+        HashMap<String, ArrayList<Integer>> fileContents = new HashMap<>();
+        ArrayList<Integer> currentFile = null;
+
+        for (File file : files) {
+            if (file.isFile()) {
+                String fileName = file.getName();
+                fileContents.put(fileName, new ArrayList<Integer>());
+                currentFile = fileContents.get(fileName);
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        currentFile.add(Integer.parseInt(line));
+                    }
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        return fileContents;
+    }
 }
