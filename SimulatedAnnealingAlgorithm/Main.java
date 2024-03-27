8128a8c528b63872a691d8bcaa89f46e36267abd
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2024-03-27 01:35:04 +0000

    Initial Testing Main for SA Algorithm

--- SimulatedAnnealingAlgorithm/Main.java
+++ SimulatedAnnealingAlgorithm/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Algorithm: Iterative Local Search to Find the Best Route"); 
         System.out.println("Algorithm: Simulated Annealing Algorithm to Find the Best Route");
     }
 }
