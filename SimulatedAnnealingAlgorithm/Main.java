de9cd3d6be87056c2721ca0b41b69dc7b9a5fb6c
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2024-03-29 13:36:17 +0000

    (update) Latest Testing Main for the Algorithm

--- SimulatedAnnealingAlgorithm/Main.java
+++ SimulatedAnnealingAlgorithm/Main.java
@@ -1,54 +1,41 @@
 import java.util.List;
 
+/**
+ * @file Main.java
+ * 
+ * @author Selepe Sello
+ * @date 29 March 2021
+ * @version 1.0
+ * @brief The Main class to run the Simulated Annealing algorithm.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        // Iterated Local Search (ILS) Algorithm
-        SimulatedAnnealing ils = new SimulatedAnnealing();
-        SolutionDetails detailsILS = ils.run();
-        int totalDistanceILS = 0;
-        int numSolutionsILS = detailsILS.getAllSolutions().size();
-        int bestDistanceILS = Integer.MAX_VALUE;
-        long totalRuntimeILS = 0;
-        int[] bestRouteILS = null;
+        // Iterated Local Search (SA) Algorithm
+        SimulatedAnnealing sa = new SimulatedAnnealing();
+        SolutionDetails detailsSA = sa.run();
+        double totalDistanceSA = 0;
+        int bestDistanceSA = Integer.MAX_VALUE;
+        double totalRuntimeSA = 0;
+        String bestRouteSA = "null";
     
-        // Calculate ILS statistics
-        for (Solution solution : detailsILS.getAllSolutions()) {
-            totalDistanceILS += solution.getDistance();
-            bestDistanceILS = Math.min(bestDistanceILS, solution.getDistance());
-            totalRuntimeILS += solution.getRuntime();
-            // System.out.println("Route: " + solution.getRoute() + ", Distance: " + solution.getDistance() + ", Runtime: " + solution.getRuntime());
-        }
+        // Calculate SA statistics
+        totalDistanceSA = detailsSA.getAverageDistance();
+        bestDistanceSA = detailsSA.getBestSolution().getDistance();
+        totalRuntimeSA = detailsSA.getTotalRuntime();
+        bestRouteSA = detailsSA.getBestSolution().getRouteString();
     
-        if (detailsILS.getBestSolution() != null) {
-            bestRouteILS = detailsILS.getBestSolution().getRoute().stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
-        }
-    
-        // Convert total runtime from milliseconds to seconds for ILS
-        double totalRuntimeILSSecs = totalRuntimeILS / 1000.0;
+        // Convert total runtime from milliseconds to seconds for SA
+        double totalRuntimeSASecs = totalRuntimeSA / 1000.0;
         
         // Print summary table
         System.out.println("--------------------------------------------");
-        System.out.println("| Problem Set            | ILS             |");
+        System.out.println("| Problem Set            | SA              |");
         System.out.println("|------------------------|-----------------|");
-        System.out.printf("| Best Solution(route)   | %-16s|%n", getRouteString(bestRouteILS));
-        System.out.printf("| Objective Function Val | %-16d|%n", bestDistanceILS);
-        System.out.printf("| Runtime (s)            | %-16.2f|%n", totalRuntimeILSSecs);
-        System.out.printf("| Av Obj Function        | %-16.2f|%n", (double) totalDistanceILS / numSolutionsILS);
+        System.out.printf("| Best Solution(route)   | %-16s|%n", bestRouteSA);
+        System.out.printf("| Objective Function Val | %-16d|%n", bestDistanceSA);
+        System.out.printf("| Runtime (s)            | %-16.2f|%n", totalRuntimeSASecs);
+        System.out.printf("| Av Obj Function        | %-16.2f|%n", totalDistanceSA);
         System.out.println("--------------------------------------------");
-    }
-    
-    private static String getRouteString(int[] route) {
-        if (route == null) return "N/A";
-        StringBuilder routeString = new StringBuilder("[");
-        for (int i = 0; i < route.length; i++) {
-            routeString.append(route[i]);
-            if (i < route.length - 1) {
-                routeString.append(", ");
-            }
-        }
-        routeString.append("]");
-        return routeString.toString();
     }
 }
