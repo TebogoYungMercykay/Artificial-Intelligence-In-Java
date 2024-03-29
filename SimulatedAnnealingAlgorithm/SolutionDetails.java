5a58988d3ac5a6564cce1492ab8562eee10692f3
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2024-03-29 04:50:46 +0000

    (update) Class For Storing Multiple Solutions

--- SimulatedAnnealingAlgorithm/SolutionDetails.java
+++ SimulatedAnnealingAlgorithm/SolutionDetails.java
@@ -1,26 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class representing details of multiple solutions.
+ */
 public class SolutionDetails {
-    private final Solution solution;
-    private final long runtime;
+    /** List of solutions. */
+    private final List<Solution> solutions;
 
-    public SolutionDetails(Solution solution, long runtime) {
-        this.solution = solution;
-        this.runtime = runtime;
+    /** The best solution among all solutions. */
+    private Solution bestSolution;
+
+    /**
+     * Constructor to initialize an empty solution details object.
+     */
+    public SolutionDetails() {
+        this.solutions = new ArrayList<>();
+        this.bestSolution = null;
     }
 
-    public int getDistance() {
-        return solution.getDistance();
+    /**
+     * Add a solution to the list of solutions.
+     * Update the best solution if necessary.
+     * @param solution The solution to add.
+     */
+    public void addSolution(Solution solution) {
+        this.solutions.add(solution);
+        if (bestSolution == null || solution.getDistance() < bestSolution.getDistance()) {
+            bestSolution = solution;
+        }
     }
 
-    public long getRuntime() {
-        return runtime;
+    /**
+     * Get the best solution among all solutions.
+     * @return The best solution.
+     */
+    public Solution getBestSolution() {
+        return bestSolution;
     }
 
-    public Solution getSolution() {
-        return solution;
+    /**
+     * Set the best solution among all solutions.
+     * @param bestSolution The best solution to set.
+     */
+    public void setBestSolution(Solution bestSolution) {
+        this.bestSolution = bestSolution;
     }
 
-    @Override
-    public String toString() {
-        return "Route: " + solution.getRoute() + ", Distance: " + solution.getDistance() + ", Runtime: " + runtime + "ms";
+    /**
+     * Calculate the average distance of all solutions.
+     * @return The average distance.
+     */
+    public double getAverageDistance() {
+        if (solutions.isEmpty()) return 0.0;
+        int totalDistance = 0;
+        for (Solution solution : solutions) {
+            totalDistance += solution.getDistance();
+        }
+        return (double) totalDistance / solutions.size();
+    }
+
+    /**
+     * Get a copy of the list of all solutions.
+     * @return A list containing all solutions.
+     */
+    public List<Solution> getAllSolutions() {
+        return new ArrayList<>(solutions);
+    }
+
+    /**
+     * Print all solutions.
+     */
+    public void printAllSolutions() {
+        System.out.println("All Solutions:");
+        for (int i = 0; i < solutions.size(); i++) {
+            System.out.println("Solution " + (i + 1) + ": " + solutions.get(i));
+        }
     }
 }
