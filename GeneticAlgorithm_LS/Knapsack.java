a44ea160119e6c3d1eeb25272f1356ed7a6fe478
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-22 23:55:05 +0000

    (update) Using A Random Seed For RandomNumbers

--- GeneticAlgorithm_LS/Knapsack.java
+++ GeneticAlgorithm_LS/Knapsack.java
@@ -1,55 +1,65 @@
 import java.util.ArrayList;
 
+/**
+ * The Knapsack class represents a knapsack problem.
+ */
 public class Knapsack {
 
     private int capacity;
     private ArrayList<Item> items;
 
+    /**
+     * Constructs a new Knapsack with the specified capacity and number of items.
+     *
+     * @param capacity the capacity of the knapsack
+     * @param numItems the number of items
+     */
     public Knapsack(int capacity, int numItems) {
         this.capacity = capacity;
-        items = new ArrayList<Item>();
+        this.items = new ArrayList<>();
     }
 
     /**
-     * @brief Get the capacity of the knapsack
-     * 
-     * @return capacity
+     * Returns the capacity of the knapsack.
+     *
+     * @return the capacity of the knapsack
      */
     public int getCapacity() {
         return capacity;
     }
 
     /**
-     * @brief Get the items in the knapsack
-     * 
-     * @return items
+     * Returns the items in the knapsack.
+     *
+     * @return the items in the knapsack
      */
     public ArrayList<Item> getItems() {
         return items;
     }
 
     /**
-     * @brief Add an item to the knapsack
-     * 
-     * @param item
+     * Adds an item to the knapsack.
+     *
+     * @param item the item to add
      */
     public void addItem(Item item) {
         items.add(item);
     }
 
     /**
-     * @brief Remove an item from the knapsack
-     * 
-     * @param item
+     * Removes an item from the knapsack.
+     *
+     * @param item the item to remove
      */
     public void removeItem(Item item) {
         items.remove(item);
     }
 
     /**
-     * @brief Get the total weight of the knapsack given a solution
-     * 
-     * @return totalWeight
+     * Returns the total weight of the knapsack given a solution.
+     *
+     * @param solution the solution
+     * @return the total weight of the knapsack
      */
     public double getWeight(Boolean[] solution) {
         double totalWeight = 0;
@@ -59,13 +69,13 @@ public class Knapsack {
             }
         }
         return totalWeight;
-
     }
 
     /**
-     * @brief Get the total value of the knapsack given a solution
-     * 
-     * @return totalValue
+     * Returns the total value of the knapsack given a solution.
+     *
+     * @param solution the solution
+     * @return the total value of the knapsack
      */
     public double getValue(Boolean[] solution) {
         double totalValue = 0;
@@ -78,8 +88,9 @@ public class Knapsack {
     }
 
     /**
-     * @brief Get the total weight of the items in the knapsack
-     * @return
+     * Returns the total weight of the items in the knapsack.
+     *
+     * @return the total weight of the items in the knapsack
      */
     public double getTotalWeight() {
         double totalWeight = 0;
@@ -90,8 +101,9 @@ public class Knapsack {
     }
 
     /**
-     * @brief Get the total value of the items in the knapsack
-     * @return
+     * Returns the total value of the items in the knapsack.
+     *
+     * @return the total value of the items in the knapsack
      */
     public double getTotalValue() {
         double totalValue = 0;
