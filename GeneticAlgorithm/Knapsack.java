d6fa56a6b54490a57974749babe2e0314c83e79f
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-26 20:02:33 +0000

    The Updated Testing and Classes for Algorithm

--- GeneticAlgorithm/Knapsack.java
+++ GeneticAlgorithm/Knapsack.java
@@ -1,103 +1,144 @@
 import java.util.ArrayList;
+/**
+ * @file Knapsack.java
+ * 
+ * @author Selepe Sello
+ * @date 26 April 2024
+ * @version 1.0
+ * @brief The Knapsack class represents a knapsack problem.
+ */
 
 public class Knapsack {
 
     private int capacity;
-    private ArrayList<Item> items;
+    private ArrayList<KnapsackItem> items;
 
+    /**
+     * Constructs a new Knapsack with the specified capacity.
+     *
+     * @param capacity the capacity of the knapsack
+     */
+    public Knapsack(int capacity) {
+        this.capacity = capacity;
+        this.items = new ArrayList<>();
+    }
+
+    /**
+     * Constructs a new Knapsack with the specified capacity and number of items.
+     *
+     * @param capacity the capacity of the knapsack
+     * @param numItems the numItems in the knapsack
+     */
     public Knapsack(int capacity, int numItems) {
         this.capacity = capacity;
-        items = new ArrayList<Item>();
+        this.items = new ArrayList<>();
     }
 
     /**
-     * @brief Get the capacity of the knapsack
-     * 
-     * @return capacity
+     * Constructs a new Knapsack by copying another Knapsack.
+     *
+     * @param other the Knapsack to copy
+     */
+    public Knapsack(Knapsack other) {
+        this.capacity = other.capacity;
+        this.items = new ArrayList<>(other.items);
+    }
+
+    /**
+     * Returns the capacity of the knapsack.
+     *
+     * @return the capacity of the knapsack
      */
     public int getCapacity() {
-        return capacity;
+        return this.capacity;
     }
 
     /**
-     * @brief Get the items in the knapsack
-     * 
-     * @return items
+     * Returns the items in the knapsack.
+     *
+     * @return the items in the knapsack
      */
-    public ArrayList<Item> getItems() {
-        return items;
+    public ArrayList<KnapsackItem> getItems() {
+        return this.items;
     }
 
     /**
-     * @brief Add an item to the knapsack
-     * 
-     * @param item
+     * Adds an item to the knapsack.
+     *
+     * @param item the item to add
      */
-    public void addItem(Item item) {
-        items.add(item);
+    public void addItem(KnapsackItem item) {
+        this.items.add(item);
     }
 
     /**
-     * @brief Remove an item from the knapsack
-     * 
-     * @param item
+     * Removes an item from the knapsack.
+     *
+     * @param item the item to remove
      */
-    public void removeItem(Item item) {
-        items.remove(item);
+    public void removeItem(KnapsackItem item) {
+        this.items.remove(item);
     }
 
     /**
-     * @brief Get the total weight of the knapsack given a solution
-     * 
-     * @return totalWeight
+     * Returns the total weight of the knapsack given a solution.
+     *
+     * @param solution the solution
+     * @return the total weight of the knapsack
      */
     public double getWeight(Boolean[] solution) {
         double totalWeight = 0;
-        for (int i = 0; i < items.size(); i++) {
+        for (int i = 0; i < this.items.size(); i++) {
             if (solution[i]) {
-                totalWeight += items.get(i).getWeight();
+                totalWeight += this.items.get(i).getWeight();
             }
         }
-        return totalWeight;
 
+        return totalWeight;
     }
 
     /**
-     * @brief Get the total value of the knapsack given a solution
-     * 
-     * @return totalValue
+     * Returns the total value of the knapsack given a solution.
+     *
+     * @param solution the solution
+     * @return the total value of the knapsack
      */
     public double getValue(Boolean[] solution) {
         double totalValue = 0;
-        for (int i = 0; i < items.size(); i++) {
+        for (int i = 0; i < this.items.size(); i++) {
             if (solution[i]) {
-                totalValue += items.get(i).getValue();
+                totalValue += this.items.get(i).getValue();
             }
         }
+
         return totalValue;
     }
 
     /**
-     * @brief Get the total weight of the items in the knapsack
-     * @return
+     * Returns the total weight of the items in the knapsack.
+     *
+     * @return the total weight of the items in the knapsack
      */
     public double getTotalWeight() {
         double totalWeight = 0;
-        for (int i = 0; i < items.size(); i++) {
-            totalWeight += items.get(i).getWeight();
+        for (int i = 0; i < this.items.size(); i++) {
+            totalWeight += this.items.get(i).getWeight();
         }
+
         return totalWeight;
     }
 
     /**
-     * @brief Get the total value of the items in the knapsack
-     * @return
+     * Returns the total value of the items in the knapsack.
+     *
+     * @return the total value of the items in the knapsack
      */
     public double getTotalValue() {
         double totalValue = 0;
-        for (int i = 0; i < items.size(); i++) {
-            totalValue += items.get(i).getValue();
+        for (int i = 0; i < this.items.size(); i++) {
+            totalValue += this.items.get(i).getValue();
         }
+
         return totalValue;
     }
 }
