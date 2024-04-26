44afd4cf9af261388a7929ca7fcde35d30cadd5c
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-26 14:47:57 +0000

    (doxygen) Updated The Year in the Brief

--- SimulatedAnnealingAlgorithm/SimulatedAnnealing.java
+++ SimulatedAnnealingAlgorithm/SimulatedAnnealing.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * @file SimulatedAnnealing.java
  * 
  * @author Selepe Sello
- * @date 29 March 2021
+ * @date 29 March 2024
  * @version 1.0
  * @brief Simulated Annealing algorithm for solving the campus tour problem.
  */
